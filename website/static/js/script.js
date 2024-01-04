f1ab9e329cbeb1b5b5ac454d78c1c2dc94efc475
excitedcrayon <bonganimkonto@gmail.com>
2024-01-04 11:55:31 +1000

    added responsive CSS for header navigation component

--- website/static/js/script.js
+++ website/static/js/script.js
@@ -5,12 +5,27 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 const animateMenuIconAndNavigation = () => {
+
     const menuBurgerIcon = document.querySelector('.menu_burger_icon');
     const nav = document.querySelector('nav');
 
     if ( menuBurgerIcon != undefined ){
+
         menuBurgerIcon.addEventListener('click', () => {
+
             menuBurgerIcon.classList.toggle('icon_clicked');
+
+            let navMobilePadding = nav.getAttribute('data-mobile-padding');
+
+            nav.classList.toggle('active_nav');
+
+            if (nav.style.maxHeight){
+                nav.style.maxHeight = null;
+                nav.style.paddingBottom = 0;
+            }else{
+                nav.style.maxHeight = `${nav.scrollHeight}px`;
+                nav.style.paddingBottom = `${parseInt(navMobilePadding)}rem`;
+            }
         });
     }
 }
